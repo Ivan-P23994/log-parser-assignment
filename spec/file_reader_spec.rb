2bc0ef588cffced016640e526ce93559e4728fb9
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-11-19 15:36:19 +0100

    Add invalid path FileReader spec

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -3,10 +3,18 @@ RSpec.describe LogParser::FileReader do
   let(:path) { File.expand_path("../fixtures/test.log", __FILE__) }
 
   describe "#lines" do
-    subject { file_reader.lines }
+    subject(:lines) { file_reader.lines }
 
     let(:expected_lines) { ["/home 1.1.1.1", "/about-us 2.2.2.2", "/careers 3.3.3.3"] }
 
     it { is_expected.to eql(expected_lines) }
+
+    context "with invalid path" do
+      let(:path) { "missing.log" }
+
+      it "raises error" do
+        expect { lines }.to raise_error(LogParser::Error)
+      end
+    end 
   end
 end
