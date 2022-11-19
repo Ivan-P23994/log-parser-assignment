434e7b6e10713ed118a1269cbcbec64eb4339800
Ivan Pavlovic <ivaanpavlovic@gmail.com>
2022-11-19 15:01:30 +0100

    Add FileReader lines method test

--- spec/file_reader_spec.rb
+++ spec/file_reader_spec.rb
@@ -1,3 +1,12 @@
-RSpec.describe.LogParser::FileReader do
-  subject(:file_reader) { described_class.new(path) }
+RSpec.describe LogParser::FileReader do
+  let(:file_reader) { described_class.new(path) }
+  let(:path) { File.expand_path("../fixtures/test.log", __FILE__) }
+
+  describe "#lines" do
+    subject { file_reader.lines }
+
+    let(:expected_lines) { ["/home 1.1.1.1", "/about-us 2.2.2.2", "/careers 3.3.3.3"] }
+
+    it { is_expected.to eql(expected_lines) }
+  end
 end
